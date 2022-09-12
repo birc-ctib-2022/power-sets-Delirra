29be61c57c2be02a3205d0ee7d73e3732964954e
Rasmus Freund <rasmus3141@gmail.com>
2022-09-12 21:55:26 +0200

    Implemented powerset calculator

--- src/powerset.py
+++ src/powerset.py
@@ -2,11 +2,24 @@
 
 from typing import Any
 
-
 def power(x: list[Any]) -> list[list[Any]]:
     """
     Compute the power-set of x.
 
     Returns the power-set of x as a list of lists.
     """
-    return []  # FIXME: you need to add some code here.
+    powerset = []
+    for i in range(0, 2**len(x)):
+        bit_representation = format(i, "b").zfill(len(x))
+        idx = 0
+        subset = []
+        for j in bit_representation[::-1]:
+            if j == "0":
+                idx += 1
+                continue
+            elif j == "1":
+                subset.append(x[idx])
+                idx += 1
+        powerset.append(subset)
+
+    return sorted(powerset, key = len)
