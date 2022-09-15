0ed01494a9a5b60531ad1662cf584b4272340532
Rasmus Freund <rasmus3141@gmail.com>
2022-09-15 16:21:43 +0200

    Implemented the enumerate function and restructured the for loop a bit

--- src/powerset.py
+++ src/powerset.py
@@ -9,17 +9,16 @@ def power(x: list[Any]) -> list[list[Any]]:
     Returns the power-set of x as a list of lists.
     """
     powerset = []
+
     for i in range(0, 2**len(x)):
         bit_representation = format(i, "b").zfill(len(x))
-        idx = 0
         subset = []
-        for j in bit_representation[::-1]:
-            if j == "0":
-                idx += 1
-                continue
-            elif j == "1":
+        for idx, j in enumerate(bit_representation[::-1]):
+            if j == "1":
                 subset.append(x[idx])
-                idx += 1
+            else:
+                continue
+
         powerset.append(subset)
 
     return sorted(powerset, key = len)
